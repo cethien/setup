439c7f055098ed3560af35844f60830244a4284e
cethien <borislaw.sotnikow@gmx.de>
2025-01-21 11:17:09 +0100

    reworked script

--- setup.sh
+++ setup.sh
@@ -1,73 +1,30 @@
 #!/usr/bin/env bash
 
-LX_ENV=any
+set -e
 
-# check if WSL
-if [ -n "$WSL_DISTRO_NAME" ]; then
-    LX_ENV=wsl
+DOTFILES_REPO=cethien/dotfiles
+
+if command -v apt >/dev/null 2>&1; then
+    echo "installing nala"
+    sudo apt update &&
+        sudo apt install -y nala curl &&
+        sudo nala update &&
+        sudo nala upgrade -y
 fi
 
-# check if NixOS
-if [ -e /etc/nixos ]; then
-    LX_ENV=nixos
-fi
-
-echo "installing setup for $LX_ENV"
-
-# for non nixOS
-if [ "$LX_ENV" = "any" ]; then
-    # install nala for for distros with apt and update
-    if command -v apt >/dev/null 2>&1; then
-        echo "installing nala"
-        sudo apt update &&
-            sudo apt install -y nala curl &&
-            sudo nala update &&
-            sudo nala upgrade -y
-    fi
-
-    # install nix
-    echo "installing nix"
+if ! command -v nixos-rebuild >/dev/null 2>&1; then
+    echo "installing nix package manager"
     curl -fsSL https://nixos.org/nix/install | bash /dev/stdin --no-daemon &&
-        mkdir -p $HOME/.config/nix &&
-        curl https://raw.githubusercontent.com/cethien/setup/lx/resources/nix.conf >>$HOME/.config/nix/nix.conf &&
-        source "$HOME"/.nix-profile/etc/profile.d/nix.sh
+        mkdir -p "$HOME/.config/nix" &&
+        curl https://raw.githubusercontent.com/cethien/setup/lx/resources/nix.conf >>"$HOME/.config/nix/nix.conf"
 fi
 
-# if nixos, pull nixos repo and rebuild
-if [ "$LX_ENV" = "nixos" ]; then
-    echo "setting up nixos"
-    NIXOS_SCRIPT='
-    git clone https://github.com/cethien/nixos.git $HOME/nixos &&
-        cat /etc/nixos/hardware-configuration.nix > $HOME/nixos/nixos/hardware-configuration.nix &&
-        sudo nixos-rebuild switch --flake $HOME/nixos/#pc-cethien
-    '
-    nix-shell -p git --run "$NIXOS_SCRIPT"
-fi
+echo "installing home-manager profile"
+source "$HOME/.nix-profile/etc/profile.d/nix.sh" &&
+    nix run nixpkgs#home-manager -- switch --flake github.com:"$DOTFILES_REPO#cethien@wsl" -b hm-bak-$(date +%Y%m%d-%H%M%S) --refresh
 
-RUN_SCRIPT='
-git init -b lx &&
-    git remote add origin https://github.com/cethien/dotfiles.git &&
-    git fetch origin lx &&
-    git reset --hard origin/lx &&
-    git pull --set-upstream origin lx &&
-    curl https://raw.githubusercontent.com/cethien/setup/lx/resources/config.template.json >>$HOME/.config/home-manager/config.json &&
-    nano $HOME/.config/home-manager/config.json &&
-    home-manager switch -b hm.bak
-'
-
-nix-shell -p git home-manager nano curl --run "$RUN_SCRIPT"
-
-# for wsl,
-if [ "$LX_ENV" = "wsl" ]; then
-    # add ssh-passthrough script
-
-    echo "adding ssh-passthrough script"
-    curl -fsSL https://raw.githubusercontent.com/cethien/setup/lx/resources/ssh-agent-pipe.sh | sudo tee /usr/local/bin/ssh-agent-pipe >/dev/null &&
-        sudo chmod +x /usr/local/bin/ssh-agent-pipe
-
-    # remove sudo password prompt
+if [ ! -z "$WSL_DISTRO_NAME" ]; then
     echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER" >/dev/null
-
-    # reboot
+    echo "rebooting system"
     sudo reboot
 fi
