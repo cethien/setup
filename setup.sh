c0b133a23c337e733543ada010d69159827bd044
cethien <borislaw.sotnikow@gmx.de>
2025-01-28 11:56:21 +0100

    setup(lx): fixed sourcing
    - finally

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ if ! command -v nixos-rebuild >/dev/null 2>&1; then
     echo "installing nix package manager"
     curl -fsSL https://nixos.org/nix/install | bash /dev/stdin --no-daemon &&
         mkdir -p "$HOME/.config/nix" &&
-        curl https://raw.githubusercontent.com/cethien/setup/lx/resources/nix.conf >>"$HOME/.config/nix/nix.conf"
+        curl -fsSL https://raw.githubusercontent.com/cethien/setup/lx/resources/nix.conf >>"$HOME/.config/nix/nix.conf"
 fi
 
 CONFIGURATION="$(whoami)@$(hostname)"
@@ -24,7 +24,8 @@ if [ ! -z "$WSL_DISTRO_NAME" ]; then
 fi
 
 echo "installing home-manager profile"
-$HOME/.nix-profile/bin/nix run nixpkgs#home-manager -- switch --flake github:cethien/dotfiles#"$CONFIGURATION" -b hm-bak-$(date +%Y%m%d-%H%M%S) --refresh
+source $HOME/.nix-profile/etc/profile.d/nix.sh
+nix run nixpkgs#home-manager -- switch --flake github:cethien/dotfiles#"$CONFIGURATION" -b hm-bak-$(date +%Y%m%d-%H%M%S) --refresh
 
 if [ ! -z "$WSL_DISTRO_NAME" ]; then
     echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER" >/dev/null
